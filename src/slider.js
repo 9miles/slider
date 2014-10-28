3635100f487fb14562bd666aace3e337eb870435
Joel Drapper <joel@drapper.com>
2014-10-28 18:17:34 +0000

    Aspect ratio sizing You can set an initial width and height, and the aspect ratio is worked out. Closes #5

--- src/slider.js
+++ src/slider.js
@@ -9,7 +9,10 @@
       settings = {
         debug: true,
         delay: 3000,
-        autoplay: true
+        autoplay: true,
+        width: 16,
+        height: 9,
+        start: 1
       };
       settings = $.extend(settings, options);
       log = function(msg) {
@@ -18,24 +21,46 @@
         }
       };
       return this.each(function() {
-        var $this, items, list, next, position, slides;
+        var $this, container, length, next, position, ratio, slides;
         $this = $(this);
-        position = 1;
-        list = $this.find('ul');
-        items = $this.find('ul li');
-        slides = items.length;
-        items.css('width', "" + (100 / slides) + "%");
-        list.css('width', "" + (100 * slides) + "%");
+        container = $this.find('ul');
+        slides = $this.find('ul li');
+        length = slides.length;
+        ratio = settings.height / settings.width * 100;
+        if (settings.start <= length) {
+          position = settings.start;
+        } else {
+          position = 1;
+        }
+        $this.css({
+          'width': '100%',
+          'overflow': 'hidden',
+          'height': 0,
+          'padding-bottom': "" + ratio + "%",
+          'position': 'relative'
+        });
+        container.css({
+          'width': "" + (100 * length) + "%",
+          'height': '100%',
+          'position': 'absolute',
+          'transition': '.4s ease all'
+        });
+        slides.css({
+          'width': "" + (100 / length) + "%",
+          'height': '100%',
+          'display': 'block',
+          'float': 'left'
+        });
         next = function() {
-          if (position < slides) {
+          if (position < length) {
             position = position + 1;
           } else {
             position = 1;
           }
           if (Modernizr.csstransforms && Modernizr.csstransitions) {
-            return list.css('transform', "translateX(-" + (100 / slides * (position - 1)) + "%)");
+            return container.css('transform', "translateX(-" + (100 / length * (position - 1)) + "%)");
           } else {
-            return list.animate('left', "-" + (100 * (position - 1)) + "%");
+            return container.animate('left', "-" + (100 * (position - 1)) + "%");
           }
         };
         return $this.click(function(event) {
