6d88feec86b3eabff7a2697e0ef46b3290d8e0a9
Joel Drapper <joel@drapper.com>
2014-10-29 12:50:13 +0000

    Next / Previous Methods
    Closes #4

--- slider.coffee
+++ slider.coffee
@@ -1,65 +1,89 @@
 $ = jQuery
 
 $.fn.extend
-	slider: (options) ->
+	slider: (options, target) ->
+
 		# Default Settings
 		settings =
-			debug: true
 			delay: 3000
 			autoplay: true
 			width: 16
 			height: 9
 
-		if typeof options == 'object'
-			settings = $.extend settings, options
-		else if options == 'next'
-			do next
-		else if options == 'prev' or options == 'previous'
-			do prev
+		# Methods
+		switch options
 
-		log = (msg) ->
-			console?.log msg if settings.debug
+			# Go
+			when 'go'
+				return @each () ->
+					$this = $(@)
+					length = $this.data('length')
+					container = $this.find('.slider-items')
+					if target <= length
+						if Modernizr.csstransforms and Modernizr.csstransitions
+							container.css('transform', "translateX(-#{100 / length * (target-1)}%)")
+						else
+							container.animate('left', "-#{100 * (target-1)}%")
+						$this.data('position', target)
 
-		return @each () ->
+			# Next
+			when 'next'
+				return @each () ->
+					$this = $(@)
+					length = $this.data('length')
+					position = $this.data('position')
 
-			$this = $(@)
-			container = $this.find('.slider-items')
-			slide = $this.find('.slider-item')
-			length = slide.length
-			ratio = settings.height / settings.width * 100
-			position = 1
-
-			$this.css
-				'width': '100%'
-				'padding-bottom': "#{ratio}%"
-
-			container.css
-				'width': "#{100*length}%"
-
-			slide.css
-				'width': "#{100/length}%"
-
-			go = (target) ->
-				if target <= length
-					if Modernizr.csstransforms and Modernizr.csstransitions
-						container.css('transform', "translateX(-#{100 / length * (target-1)}%)")
+					if position < length
+						$this.slider('go', position + 1)
 					else
-						container.animate('left', "-#{100 * (target-1)}%")
-					position = target
+						$this.slider('go', 1)
 
-			next = ->
-				if position < length then go position + 1 else go 1
+			# Previous
+			when 'prev' or 'previous'
+				return @each () ->
+					$this = $(@)
+					length = $this.data('length')
+					position = $this.data('position')
 
-			prev = ->
-				if position > 1 then go position - 1 else go length
+					if position > 1
+						$this.slider('go', position - 1)
+					else
+						$this.slider('go', length)
 
-			# Temporary measure to run the next function on click
-			$this.click (event) ->
-				do event.preventDefault
-				do next
+			# Initialise
+			else
 
-			# Autoplay
-			if settings.autoplay
-				setInterval ->
-					do next
-				, settings.delay
+				# Merge default settings with options.
+				settings = $.extend settings, options
+
+				return @each () ->
+
+					$this = $(@)
+					container = $this.find('.slider-items')
+					slide = $this.find('.slider-item')
+					length = slide.length
+					$this.data('length', length)
+					ratio = settings.height / settings.width * 100
+					position = 1
+					$this.data('position', position)
+
+					$this.css
+						'width': '100%'
+						'padding-bottom': "#{ratio}%"
+
+					container.css
+						'width': "#{100*length}%"
+
+					slide.css
+						'width': "#{100/length}%"
+
+					# Temporary measure to run the next function on click
+					$this.click (event) ->
+						do event.preventDefault
+						$this.slider('next')
+
+					# Autoplay
+					if settings.autoplay
+						setInterval ->
+							$this.slider('next')
+						, settings.delay
--- src/slider.js
+++ src/slider.js
@@ -4,79 +4,87 @@
   $ = jQuery;
 
   $.fn.extend({
-    slider: function(options) {
-      var log, settings;
+    slider: function(options, target) {
+      var settings;
       settings = {
-        debug: true,
         delay: 3000,
         autoplay: true,
         width: 16,
         height: 9
       };
-      if (typeof options === 'object') {
-        settings = $.extend(settings, options);
-      } else if (options === 'next') {
-        next();
-      } else if (options === 'prev' || options === 'previous') {
-        prev();
-      }
-      log = function(msg) {
-        if (settings.debug) {
-          return typeof console !== "undefined" && console !== null ? console.log(msg) : void 0;
-        }
-      };
-      return this.each(function() {
-        var $this, container, go, length, next, position, prev, ratio, slide;
-        $this = $(this);
-        container = $this.find('.slider-items');
-        slide = $this.find('.slider-item');
-        length = slide.length;
-        ratio = settings.height / settings.width * 100;
-        position = 1;
-        $this.css({
-          'width': '100%',
-          'padding-bottom': "" + ratio + "%"
-        });
-        container.css({
-          'width': "" + (100 * length) + "%"
-        });
-        slide.css({
-          'width': "" + (100 / length) + "%"
-        });
-        go = function(target) {
-          if (target <= length) {
-            if (Modernizr.csstransforms && Modernizr.csstransitions) {
-              container.css('transform', "translateX(-" + (100 / length * (target - 1)) + "%)");
-            } else {
-              container.animate('left', "-" + (100 * (target - 1)) + "%");
+      switch (options) {
+        case 'go':
+          return this.each(function() {
+            var $this, container, length;
+            $this = $(this);
+            length = $this.data('length');
+            container = $this.find('.slider-items');
+            if (target <= length) {
+              if (Modernizr.csstransforms && Modernizr.csstransitions) {
+                container.css('transform', "translateX(-" + (100 / length * (target - 1)) + "%)");
+              } else {
+                container.animate('left', "-" + (100 * (target - 1)) + "%");
+              }
+              return $this.data('position', target);
             }
-            return position = target;
-          }
-        };
-        next = function() {
-          if (position < length) {
-            return go(position + 1);
-          } else {
-            return go(1);
-          }
-        };
-        prev = function() {
-          if (position > 1) {
-            return go(position - 1);
-          } else {
-            return go(length);
-          }
-        };
-        $this.click(function(event) {
-          event.preventDefault();
-          return next();
-        });
-        if (settings.autoplay) {
-          return setInterval(function() {
-            return next();
-          }, settings.delay);
-        }
-      });
+          });
+        case 'next':
+          return this.each(function() {
+            var $this, length, position;
+            $this = $(this);
+            length = $this.data('length');
+            position = $this.data('position');
+            if (position < length) {
+              return $this.slider('go', position + 1);
+            } else {
+              return $this.slider('go', 1);
+            }
+          });
+        case 'prev' || 'previous':
+          return this.each(function() {
+            var $this, length, position;
+            $this = $(this);
+            length = $this.data('length');
+            position = $this.data('position');
+            if (position > 1) {
+              return $this.slider('go', position - 1);
+            } else {
+              return $this.slider('go', length);
+            }
+          });
+        default:
+          settings = $.extend(settings, options);
+          return this.each(function() {
+            var $this, container, length, position, ratio, slide;
+            $this = $(this);
+            container = $this.find('.slider-items');
+            slide = $this.find('.slider-item');
+            length = slide.length;
+            $this.data('length', length);
+            ratio = settings.height / settings.width * 100;
+            position = 1;
+            $this.data('position', position);
+            $this.css({
+              'width': '100%',
+              'padding-bottom': "" + ratio + "%"
+            });
+            container.css({
+              'width': "" + (100 * length) + "%"
+            });
+            slide.css({
+              'width': "" + (100 / length) + "%"
+            });
+            $this.click(function(event) {
+              event.preventDefault();
+              return $this.slider('next');
+            });
+            if (settings.autoplay) {
+              return setInterval(function() {
+                return $this.slider('next');
+              }, settings.delay);
+            }
+          });
+      }
     }
   });
 
